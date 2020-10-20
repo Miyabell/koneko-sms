f7e9053d1d5fea2b3d6657d7cd098cd4f47625bc
Miyabell <alsa1594@gmail.com>
2020-10-20 19:45:22 +0200

    asd

--- public/activity.js
+++ public/activity.js
@@ -1,6 +1,6 @@
 var connection = new Postmonger.Session();
 // Getting configuration file
-var configFile = require('activity.json');
+var configFile = require('./public/activity.json');
 //var configuration1 = JSON.stringify(configFile);
 
 /*
