7be86e4ee62a463854350b40fbd006ec3f91e691
Miyabell <alsa1594@gmail.com>
2020-10-20 18:49:23 +0200

    eargvbf

--- test.js
+++ test.js
@@ -1,7 +1,48 @@
 // TESTING FILE
 //   NODE TEST.JS IN CMD
 
-var configFile = require('./demo-data/activity.json');
-var configuration = JSON.stringify(configFile);
+var config = `{
+                "name": "",
+                "id": null,
+                "key": "REST-1",
+                "arguments": {
+                  "execute": {
+                    "inArguments": [
+                      {"contactKey": "{{Contact.Key}}"},
+                      {"first_name": "{{Contact.Attribute.bg_marketing_test.first_name}}"},
+                      {"last_name": "{{Contact.Attribute.bg_marketing_test.last_name}}"},
+                      {"email": "{{Contact.Attribute.bg_marketing_test.email}}"},
+                      {"phone": "{{Contact.Attribute.bg_marketing_test.phone}}"}
+                    ],
+                    "url": "https://enz3kkxiw5fpq.x.pipedream.net/"
+                  }
+                },
+                "configurationArguments": {
+                  "publish": {
+                    "url": "https://enz3kkxiw5fpq.x.pipedream.net/"
+                  }
+                },
+                "metaData": {
+                  "icon": "https://koneko-mc.herokuapp.com/icon.png",
+                  "category": "message",
+                  "iconSmall": null,
+                  "statsContactIcon": null,
+                  "original_icon": "icon.png",
+                  "isConfigured": true,
+                  "configurationDisabled": true
+                },
+                "editable": true,
+                "outcomes": [
+                  {
+                    "key": "ba80c3b0-7c5c-4c83-a582-5d230f242910",
+                    "next": "WAITBYDURATION-2",
+                    "arguments": {},
+                    "metaData": {
+                      "invalid": false
+                    }
+                  }
+                ],
+                "errors": []
+              }`;
 
-console.log(configuration);
+console.log(config);
