78696e4696a420e9e5153e717f0a08f4469ca09d
Miyabell <alsa1594@gmail.com>
2020-10-21 12:58:14 +0200

    Updated UI

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var port = process.env.PORT || 8080;
 //Serve static files
 app.use(express.static('public'));
 
-console.log('Served: http://localhost' + port);
+console.log('Served: http://localhost:' + port);
 app.listen(port);
