de7d64ae97119c960ae9d82a04af13ecb18947a3
Miyabell <alsa1594@gmail.com>
2020-10-21 18:21:13 +0200

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -1,51 +1,6 @@
 var connection = new Postmonger.Session();
 // Getting configuration file
-var configFile = require('../public/activity.json');
-
-
-/* var config = {
-                "name": "",
-                                  "id": null,
-                                  "key": "REST-1",
-                                  "arguments": {
-                                    "execute": {
-                                      "inArguments": [
-                                        {"contactKey": "{{Contact.Key}}"},
-                                        {"first_name": "{{Contact.Attribute.bg_marketing_test.first_name}}"},
-                                        {"last_name": "{{Contact.Attribute.bg_marketing_test.last_name}}"},
-                                        {"email": "{{Contact.Attribute.bg_marketing_test.email}}"},
-                                        {"phone": "{{Contact.Attribute.bg_marketing_test.phone}}"}
-                                      ],
-                                      "url": "https://enz3kkxiw5fpq.x.pipedream.net/"
-                                    }
-                                  },
-                                  "configurationArguments": {
-                                    "publish": {
-                                      "url": "https://enz3kkxiw5fpq.x.pipedream.net/"
-                                    }
-                                  },
-                                  "metaData": {
-                                    "icon": "https://koneko-mc.herokuapp.com/icon.png",
-                                    "category": "message",
-                                    "iconSmall": null,
-                                    "statsContactIcon": null,
-                                    "original_icon": "icon.png",
-                                    "isConfigured": true,
-                                    "configurationDisabled": true
-                                  },
-                "editable": true,
-                "outcomes": [
-                  {
-                    "key": "ba80c3b0-7c5c-4c83-a582-5d230f242910",
-                    "next": "WAITBYDURATION-2",
-                    "arguments": {},
-                    "metaData": {
-                      "invalid": false
-                    }
-                  }
-                ],
-                "errors": []
-              }; */
+var configFile = require('activity.json');
 
 //Startup Sequence
 connection.trigger('ready');
@@ -60,16 +15,16 @@ connection.on('clickedNext', function() {
     // Getting and parsing content of textarea
 	//var testMSG = document.getElementById('message').value;
 	// Adding new value to payload
-	var testMSG = "Test";
+	//var testMSG = "Test";
 	// Modyfing payload
-	configFile['arguments'].execute.inArguments = [{
+	/*configFile['arguments'].execute.inArguments = [{
                 "SMSmessage": testMSG,
                 "contactKey": "{{Contact.Key}}",
                 "first_name": "{{Contact.Attribute.bg_marketing_test.first_name}}",
                 "last_name": "{{Contact.Attribute.bg_marketing_test.last_name}}",
                 "email": "{{Contact.Attribute.bg_marketing_test.email}}",
                 "phone": "{{Contact.Attribute.bg_marketing_test.phone}}"
-                }];
+                }];*/
 
     //Saving configuration
 	connection.trigger('updateActivity', configFile);
