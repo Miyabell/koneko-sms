a915eca58bb87f5cb72488cbe4e479e1d6c4b042
Miyabell <alsa1594@gmail.com>
2020-10-21 11:46:43 +0200

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
-//setting middleware
+//Serve static files
 app.use(express.static('public'));
 
-
-var server = app.listen(process.env.PORT || 5000);
+console.log('Served: http://localhost' + port);
+app.listen(port);
